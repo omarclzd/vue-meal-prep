f5f1532588dbb71d061b0353c5ceb385492ad8d3
omar <omasrt@gmail.com>
2019-07-06 18:14:49 -0500

    add vuex state and action for api

--- src/store.js
+++ src/store.js
@@ -1,10 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import { stat } from 'fs';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: 'bae0f9ff',
+                        app_key: '9dd3716ead5873acd617566a1d11911d',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    }
 });
