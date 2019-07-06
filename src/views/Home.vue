eebb4c5a6f19cf6d3345a290b4f316bd1171f667
omar <omasrt@gmail.com>
2019-07-06 16:39:25 -0500

    add app-navigation compnent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,7 @@
 <template>
-  <HelloWorld />
+    
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
 </script>
