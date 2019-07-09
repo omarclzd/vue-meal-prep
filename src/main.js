b6ce2285db34e081e2e8fe8e237fa7ce47638c9d
omar <omasrt@gmail.com>
2019-07-09 14:17:08 -0500

    updates firebase

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@/utils/';
+// import '@/utils/firebase';
 
 Vue.config.productionTip = false;
 
